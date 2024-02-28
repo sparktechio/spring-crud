db5fc60604cf789b95eb64c694e8ba9e7b2369c1
Akif Hadziabdic <akif.h@toptal.com>
2024-02-28 23:03:15 +0100

    Entity graph support

--- src/main/java/com/sparktechcode/springcrud/controllers/common/SimpleLongIdFullCrudController.java
+++ src/main/java/com/sparktechcode/springcrud/controllers/common/SimpleLongIdFullCrudController.java
@@ -3,6 +3,6 @@ package com.sparktechcode.springcrud.controllers.common;
 import com.sparktechcode.springcrud.controllers.multiple.SCRUDController;
 import com.sparktechcode.springjpasearch.entities.BaseEntity;
 
-public interface SimpleLongIdFullCrudController<Entity extends BaseEntity<String>> extends
-        SCRUDController<String, Entity, Entity, Entity> {
+public interface SimpleLongIdFullCrudController<Entity extends BaseEntity<Long>> extends
+        SCRUDController<Long, Entity, Entity, Entity> {
 }
--- src/main/java/com/sparktechcode/springcrud/controllers/common/SimpleStringIdFullCrudController.java
+++ src/main/java/com/sparktechcode/springcrud/controllers/common/SimpleStringIdFullCrudController.java
@@ -3,6 +3,6 @@ package com.sparktechcode.springcrud.controllers.common;
 import com.sparktechcode.springcrud.controllers.multiple.SCRUDController;
 import com.sparktechcode.springjpasearch.entities.BaseEntity;
 
-public interface SimpleStringIdFullCrudController<Entity extends BaseEntity<Long>> extends
-        SCRUDController<Long, Entity, Entity, Entity> {
+public interface SimpleStringIdFullCrudController<Entity extends BaseEntity<String>> extends
+        SCRUDController<String, Entity, Entity, Entity> {
 }
