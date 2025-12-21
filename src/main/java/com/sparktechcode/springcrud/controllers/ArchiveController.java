8198fae88f941f03ef41a9b4b6f5436f9df62cd3
Akif Hadziabdic <akifh@spark-tech.io>
2025-12-21 22:54:04 +0100

    Fix order issue

--- src/main/java/com/sparktechcode/springcrud/controllers/ArchiveController.java
+++ src/main/java/com/sparktechcode/springcrud/controllers/ArchiveController.java
@@ -5,6 +5,7 @@ import com.sparktechcode.springcrud.entities.BaseArchiveEntity;
 import com.sparktechcode.springcrud.payloads.PathParams;
 import com.sparktechcode.springjpasearch.entities.IdHolder;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -15,9 +16,9 @@ public interface ArchiveController<Id extends Serializable, Entity extends BaseA
         ArchiveAction<Id, Entity, Response> {
 
     @Transactional()
-    @PutMapping("{id}")
+    @DeleteMapping("{id}")
     default Response archive(
-            @PathVariable String id,
+            @PathVariable Id id,
             @PathVariable(required = false) Map<String, String> pathParams
     ) {
         return archive(id, PathParams.of(pathParams));
